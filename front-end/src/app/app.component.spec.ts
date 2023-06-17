44c67bb02bc75f6db99df22575339fabf041326d
achllx <zumarchel@gmail.com>
2023-06-17 11:37:51 +0700

    restructure repository

--- front-end/src/app/app.component.spec.ts
+++ front-end/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'password-manager'`, () => {
+  it(`should have as title 'front-end'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('password-manager');
+    expect(app.title).toEqual('front-end');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('password-manager app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('front-end app is running!');
   });
 });
